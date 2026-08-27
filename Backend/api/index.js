2c9954bba2f1940a509cfd4bc96f61bf2f9575e8
Jatin Gupta <animatedworld434@gmail.com>
2026-08-27 14:28:25 +0530

    File edited

--- Backend/api/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const app = require('../src/app');
-const connectDB = require('../src/db/db');
-
-connectDB();
-
-module.exports = app;
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,20 +3,14 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.route')
 const musicRoutes = require('./routes/music.routes')
-const app = express()
 
-// const app = express()
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-//     credentials: true
-// }))
+const app = express()
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
 
 app.use('/api/auth', authRoutes)
 app.use('/api/music', musicRoutes)
